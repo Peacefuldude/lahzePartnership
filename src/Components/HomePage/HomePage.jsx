19ab40a17891b87d52cebe2e2ba93f198f643ed9
Elahe Yaghoubi <elaheyaghoobi1@gmail.com>
2025-05-05 22:41:01 +0330

    features section added

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,14 +2,17 @@ import React, {Fragment, Suspense} from 'react'
 import GiftCardSection from "./GiftCardSection.jsx";
 import FeaturedBrandsSection from "./FeaturedBrandsSection.jsx";
 import BenefitsSection from "./BenefitsSection.jsx";
-
+import FeaturesSection from "./FeaturesSection.jsx";
 function HomePage() {
     return (
         <div>
             <GiftCardSection/>
             <FeaturedBrandsSection/>
             <BenefitsSection/>
-            <BenefitsSection/>
+            <FeaturesSection />
+            <div>
+                <img className={"w-full"} src="../src/assets/image 4.svg" alt=""/>
+            </div>
         </div>
     )
 }
