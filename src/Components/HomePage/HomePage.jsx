508c4d9d8ce7dba7a4a7027e8812c166906b38da
Elahe Yaghoubi <elaheyaghoobi1@gmail.com>
2025-05-05 21:13:10 +0330

    benefits section added

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React, {Fragment, Suspense} from 'react'
 import GiftCardSection from "./GiftCardSection.jsx";
 import FeaturedBrandsSection from "./FeaturedBrandsSection.jsx";
+import BenefitsSection from "./BenefitsSection.jsx";
 
 function HomePage() {
     return (
         <div>
             <GiftCardSection/>
             <FeaturedBrandsSection/>
+            <BenefitsSection/>
         </div>
     )
 }
